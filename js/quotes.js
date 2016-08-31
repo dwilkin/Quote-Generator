0e486b5199a79c20dbbcf45ef93cc1b203cc7261
dwilkin <darian@wilkin.online>
2016-08-31 00:55:09 -0700

    First Draft
    The bones are all working here and it could be turned in. Need to add
    actual quotes and prioritize extra credit portions.

---
+++ js/quotes.js
@@ -0,0 +1,34 @@
+var quotes = [
+  {
+    quote: "This is an example quote",
+    source: "John Doe",
+    citation: "http://domain.com",
+    year: 1991
+  },
+  {
+    quote: "This is a different example quote",
+    source: "Tupac",
+    citation: "http://killarecords.org",
+    year: 1991
+  },
+  {
+    quote: "This is yet another example quote",
+    source: "Biggy",
+  },
+  {
+    quote: "Now we're cooking",
+    source: "Iggy Ezaliea",
+    citation: "http://enom.com",
+    year: 1991
+  },
+  {
+    quote: "Getting tedious",
+    source: "Peter Gabriel",
+    year: 1985
+  },
+  {
+    quote: "Fuck you it is not tedious",
+    source: "Jane Doe",
+    citation: "http://varsity.blues"
+  }
+]
--- js/script.js
+++ js/script.js
@@ -1,4 +1,46 @@
+
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+// Initially I had trouble with this because printQuote takes an object literal array
+// as an argument, but once I converted it to an anonymous function we were good
+document.getElementById("loadQuote").addEventListener("click", function() {printQuote(quotes);}, false);
 
+// This is a simple printing function like we've gone over in previous lessons, called later
+function print(message) {
+  var outputDiv = document.getElementById("quote-box");
+  outputDiv.innerHTML = message;
+}
+
+// getRandomQuote gets a random number based on the number of objects in an object literal array (n).
+// Typically when using Math.random you add +1 due to the inclusive zero and exclusive one,
+// but here we actually want the selection to be from zero to n-1 so no addition is needed
+function getRandomQuote(quoteList) {
+  var rand = Math.floor(Math.random() * quoteList.length);
+  return (quoteList[rand]);
+}
+
+// printQuote takes an object literal array and pulls a random object from it,
+// then dices up the object and formats it for the page.  Finally it spits it out
+// via the print function. I wanted to just have it take a single object as an argument
+// and have it run based on a global variable "rand" that getRandomQuote would update,
+// but the reqs were very specific here.
+function printQuote(quoteList) {
+  var quoteMeta = getRandomQuote(quoteList);
+  var message = "";
+
+  message = '<p class="quote">' + quoteMeta.quote + '</p>';
+  message += '<p class="source">' + quoteMeta.source;
+
+// in these two if statements we need to determine if the property exists,
+// and if it does we need to format the contents for output. If it doesn't
+// we can skip it.
+  if (quoteMeta.hasOwnProperty('citation')){
+    message += '<span class="citation">' + quoteMeta.citation + '</span>';
+  }
+  if (quoteMeta.hasOwnProperty('year')){
+    message += '<span class="year">' + quoteMeta.year + '</span>';
+  }
+
+  message += '</p>';
+  print(message);
+}
