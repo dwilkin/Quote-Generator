41a43c1b8971548e6d240047eef9bfb670abd416
Darian Wilkin <darian@wilkin.online>
2016-09-18 09:33:00 -0700

    Adding Quotes
    Gradually adding actual content to the page.  This one is all
    Shakespeare quotes.

--- js/quotes.js
+++ js/quotes.js
@@ -1,40 +1,79 @@
 var quotes = [
   {
-    quote: "This is an example quote",
-    source: "John Doe",
-    citation: "http://domain.com",
-    year: 1991,
-    tags: ["business", "pleasure"]
+    quote: "This above all: to thine own self be true",
+    source: "William Shakespeare",
+    citation: "Hamlet, Act I, Scene III",
+    year: 1603,
+    tags: ["shakespeare", "art"]
   },
   {
-    quote: "This is a different example quote",
-    source: "Tupac",
-    citation: "http://killarecords.org",
-    year: 1991,
-    tags: ["gardening"]
+    quote: "Brevity is the soul of wit",
+    source: "William Shakespeare",
+    citation: "Hamlet, Act II, Scene II",
+    year: 1603,
+    tags: ["shakespeare", "art"]
   },
   {
-    quote: "This is yet another example quote",
-    source: "Biggy",
-    tags: ["politics", "leadership"]
+    quote: "All the world 's a stage, and all the men and women merely players. They have their exits and their entrances; And one man in his time plays many parts",
+    source: "William Shakespeare",
+    citation: "As You Like It, Act II, Scene VII",
+    year: 1599,
+    tags: ["shakespeare", "art"]
   },
   {
-    quote: "Now we're cooking",
-    source: "Iggy Ezaliea",
-    citation: "http://enom.com",
-    year: 1991,
-    tags: []
+    quote: "The fool doth think he is wise, but the wise man knows himself to be a fool",
+    source: "William Shakespeare",
+    citation: "As You Like It, Act V, Scene I",
+    year: 1599,
+    tags: ["shakespeare", "art"]
   },
   {
-    quote: "Getting tedious",
-    source: "Peter Gabriel",
-    year: 1985,
-    tags: ["disco"]
+    quote: "A horse! a horse! my kingdom for a horse!",
+    source: "William Shakespeare",
+    citation: "King Richard III, Act V, Scene IV",
+    year: 1591,
+    tags: ["shakespeare", "art"]
   },
   {
-    quote: "Fuck you it is not tedious",
-    source: "Jane Doe",
-    citation: "http://varsity.blues",
-    tags: ["television", "movies", "actors"]
+    quote: "What's in a name? That which we call a rose by any other name would smell as sweet",
+    source: "William Shakespeare",
+    citation: "Romeo and Juliet, Act II, Scene II",
+    year: 1595,
+    tags: ["shakespeare", "art"]
+  },
+  {
+    quote: "Tempt not a desperate man",
+    source: "William Shakespeare",
+    citation: "Romeo and Juliet, Act V, Scene III",
+    year: 1595,
+    tags: ["shakespeare", "art"]
+  },
+  {
+    quote: "If you prick us, do we not bleed? if you tickle us, do we not laugh? if you poison us, do we not die? and if you wrong us, shall we not revenge?",
+    source: "William Shakespeare",
+    citation: "The Merchant of Venice, Act III, Scene I",
+    year: 1596,
+    tags: ["shakespeare", "art"]
+  },
+  {
+    quote: "The devil can cite Scripture for his purpose",
+    source: "William Shakespeare",
+    citation: "The Merchant of Venice, Act I, Scene III",
+    year: 1596,
+    tags: ["shakespeare", "art"]
+  },
+  {
+    quote: "Some rise by sin, and some by virtue fall",
+    source: "William Shakespeare",
+    citation: "Measure for Measure, Act II, Scene I",
+    year: 1623,
+    tags: ["shakespeare", "art"]
+  },
+  {
+    quote: "The better part of valour is discretion",
+    source: "William Shakespeare",
+    citation: "King Henry IV, Part I, Act V, Scene IV",
+    year: 1596,
+    tags: ["shakespeare", "art"]
   }
 ]
