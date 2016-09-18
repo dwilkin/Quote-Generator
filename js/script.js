a6d5b17e94d6f0c1cb35a1bb1a9265a5f9f65c00
Darian Wilkin <darian@wilkin.online>
2016-09-17 18:02:19 -0700

    Screw the menu
    We'll work on the menu when I'm more comfortable with this stuff, right
    now it's just a bit over my head and it was making me lose focus on the
    project.  I've completed all tasks and extra tasks from the Project 1
    requirements, going to submit once I actually populate with real quotes

--- js/script.js
+++ js/script.js
@@ -4,8 +4,12 @@
 // Initially I had trouble with this because printQuote takes an object literal array
 // as an argument, but once I converted it to an anonymous function we were good
 document.getElementById("loadQuote").addEventListener("click", function() {printQuote(quotes);}, false);
-document.getElementById("add-button").addEventListener("click", toggleAddForm, true);
-var addFormOpen = false;
+
+// Show a new quote every 30 seconds.  This doesn't base it on clicks of the button, which we fix
+// the first time the button is clicked.
+var interval = window.setInterval(function(){printQuote(quotes);}, 30000);
+
+var usedQuotes = [];
 
 // This is a simple printing function
 function print(message) {
@@ -16,9 +20,39 @@ function print(message) {
 // getRandomQuote gets a random number based on the number of objects (n) in an object literal array.
 // Typically when using Math.random you add +1 due to the inclusive zero and exclusive one,
 // but here we actually want the selection to be from zero to n-1 so no addition is needed
+// As an addition, it also keeps track of the used quotes and ensures no quote is used twice before
+// every quote is used once.
 function getRandomQuote(quoteList) {
-  var rand = Math.floor(Math.random() * quoteList.length);
+  // Get a random quote and make sure it hasn't been used yet
+  do {
+    var rand = Math.floor(Math.random() * quoteList.length);
+  } while(usedQuotes.indexOf(rand) >= 0)
+
+  // at this point we know the quote hasn't been used yet, so we add it to the "used" list as we're about to use it
+  usedQuotes.push(rand);
+
+  // Leaving this in for easy verification that we're going through every quote
+  console.log(usedQuotes);
+
+  // if we've used all of the quotes in the list we need to wipe the slate clean
+  if (usedQuotes.length === quoteList.length) {
+    usedQuotes = [];
+  }
+
   return (quoteList[rand]);
+}
+
+// getRandomColor returns a string in the format of a hex color.  This gave me a bit of trouble
+// because initially I wanted to just use the (255,255,255) format, but that doesn't work with
+// the backgroundColor property (or at least not how I expected), which was an annoying thing to figure out.
+function getRandomColor() {
+  function getColor(){
+    // pick a number between 0 and 255, inclusive. Because math.random is exclusive of 1, we include 256 which can't be reached.
+    var color = Math.floor(Math.random() * 256);
+    //convert the 0-255 number to hex
+    return color.toString(16);
+  }
+  return ("#"+getColor()+getColor()+getColor());
 }
 
 // printQuote takes an object literal array and pulls a random object from it,
@@ -27,8 +61,13 @@ function getRandomQuote(quoteList) {
 // and have it run based on a global variable "rand" that getRandomQuote would update,
 // but the reqs were very specific here.
 function printQuote(quoteList) {
+  // First, clear the timer so it doesn't refresh the quote based on page load rather than button click
+  window.clearInterval(interval)
   var quoteMeta = getRandomQuote(quoteList);
   var message = "";
+
+  // Randomly change the background color when the quote changes
+  document.body.style.backgroundColor = getRandomColor();
 
   message = '<p class="quote">' + quoteMeta.quote + '</p>';
   message += '<p class="source">' + quoteMeta.source;
@@ -44,20 +83,9 @@ function printQuote(quoteList) {
   }
 
   message += '</p>';
+  // And we display the total message
   print(message);
-}
 
-function toggleAddForm() {
-  event.stopPropagation();
-  var quoteBox = document.getElementById("quote-box");
-  var menuForm = document.getElementById("menu-form")
-  if (addFormOpen === false){
-    quoteBox.style.top = "40%";
-    menuForm.style.display = "block";
-    addFormOpen = true;
-  } else {
-    quoteBox.style.top = "20%";
-    menuForm.style.display = "none";
-    addFormOpen = false;
-  }
+  // Set the timeout so that after this function runs each time, it re-runs in 30 secons if it hasn't yet
+  interval = window.setTimeout(function(){printQuote(quotes);}, 30000);
 }
